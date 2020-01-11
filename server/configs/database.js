21cc59ec10c34658f048567920330e81109fdb0d
Yura R <chepa92@gmail.com>
2020-01-11 15:52:47 +0200

    fix db compile warning

--- server/configs/database.js
+++ server/configs/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI || `'mongodb://localhost/'`;
 
 mongoose
-  .connect(uri, { useNewUrlParser: true })
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`

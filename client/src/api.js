08e804a8470f22f1e8354adff4e0f9fc5d23cec1
Yura R <chepa92@gmail.com>
2020-01-11 15:08:59 +0200

    fix port

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api' : '/api',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? '/api'
+      : 'http://localhost:5000/api',
   withCredentials: true,
 });
 

6f31dc8b9011dea0954c058b5f7d8fe30f4f35c5
poli10034 <poli10034@gmail.com>
2020-01-12 14:27:24 +0200

    Creating User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const coupon = require('./Coupon');
+
+const genderValues = ['Male', 'Female'];
+
+const rankLevel = ['firstLevel', 'secondLevel', 'therdLevel'];
 
 const userSchema = new Schema(
   {
-    username: String,
-    password: String,
+    username: {
+      type: String,
+      required: true,
+      index: 1,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: String,
+    age: Number,
+    gender: {
+      type: String,
+      enum: genderValues,
+    },
+    img: String,
+    orders: [coupon],
   },
   {
     timestamps: {
       createdAt: 'created_at',
       updatedAt: 'updated_at',
     },
-  }
+  },
+  { collection: 'users' }
 );
 
 const User = mongoose.model('User', userSchema);

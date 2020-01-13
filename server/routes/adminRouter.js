f1a8032cffbddb233e955347e488cbdf48c132be
CHEPA <chepa@bigeek.net>
2020-01-13 20:50:32 +0200

    KPN-22 - define MODEL of permission (models folder) to manage API

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -2,33 +2,11 @@ const express = require('express');
 const { isLoggedIn } = require('../middlewares');
 const router = express.Router();
 
-const Permission = require('../models/Permission');
-
 router.get('/secret', isLoggedIn, (req, res, next) => {
   res.json({
     secret: 4242421111,
     user: req.user,
   });
-});
-
-router.get('/permissions', async (req, res, next) => {
-  Permission.find()
-    .then(permisions => {
-      res.json(permisions);
-    })
-    .catch(err => next(err));
-});
-
-router.post('/permissions', isLoggedIn, (req, res, next) => {
-  let { role, path } = req.body;
-  Permission.create({ role, path })
-    .then(permisions => {
-      res.json({
-        success: true,
-        permisions,
-      });
-    })
-    .catch(err => next(err));
 });
 
 module.exports = router;

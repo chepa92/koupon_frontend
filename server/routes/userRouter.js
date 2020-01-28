5ac6c671797cf5df2734be3bf43291db6ff2cb66
Yura R <chepa92@gmail.com>
2020-01-28 19:44:50 +0200

    fix

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/getAllUsers', userController.getAll);
-router.get('/user', userController.getUser);
+router.get('/getUser', userController.getUser);
 router.put('/updateUser', userController.updateUser);
 router.put('/suspendUser', userController.suspendUser);
 router.post('/user', userController.addUser);
 router.delete('/deleteUser', userController.getAll);
 
-module.exports = router
+module.exports = router;

5f3793e3ed4786f0a2876fe88fca1c393d545c78
Yura R <chepa92@gmail.com>
2020-03-01 00:58:55 +0200

    server/client seperation

--- client/src/components/pages/Requests.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import RequestsTable from '../Requests/RequestsTable';
-
-export default function Requests() {
-  return <RequestsTable />;
-}
---
+++ src/components/pages/Requests.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RequestsTable from '../Requests/RequestsTable';
+
+export default function Requests() {
+  return <RequestsTable />;
+}
